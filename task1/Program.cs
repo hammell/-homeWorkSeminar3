ac073c464b24c8127ab84a37bb18d929d806ff59
Alexandr <hammell@ya.ru>
2022-09-12 23:41:12 +0800

    twoTask

---
+++ task1/Program.cs
@@ -0,0 +1,25 @@
+﻿double GetDistanceTwoPoint(int x1, int y1, int z1, int x2, int y2, int z2)
+{
+    double distanse = Math.Sqrt(Math.Pow(x1 - x2, 2) + Math.Pow(y1 - y2, 2) + Math.Pow(z1 - z2, 2));
+    return distanse;
+}
+Console.Write("Введите X1: ");
+int x1 = int.Parse(Console.ReadLine());
+
+Console.Write("Введите Y1: ");
+int y1 = int.Parse(Console.ReadLine());
+
+Console.Write("Введите Z1: ");
+int z1 = int.Parse(Console.ReadLine());
+
+Console.Write("Введите X2: ");
+int x2 = int.Parse(Console.ReadLine());
+
+Console.Write("Введите Y2: ");
+int y2 = int.Parse(Console.ReadLine());
+
+Console.Write("Введите Z2: ");
+int z2 = int.Parse(Console.ReadLine());
+
+double result = GetDistanceTwoPoint(x1, y1, z1, x2, y2, z2);
+Console.WriteLine($"Расстояние между точками ({x1}, {y1}, {z1}) и ({x2}, {y2}, {z2}) равно  {Math.Round(result, 2)}");
---
+++ task2/Program.cs
@@ -0,0 +1,10 @@
+﻿//Напишите программу, которая принимает на вход число (N) и выдаёт таблицу кубов чисел от 1 до N.
+
+
+Console.Write("Введите n: ");
+int n = int.Parse(Console.ReadLine());
+
+for (int i = 1; i <= n; i++)
+{
+    Console.WriteLine(i * i *i);
+}
